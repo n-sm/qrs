4496591df474522ac0c26945cf10d84034824c46
smnicolas <smnicolas@gmail.com>
2015-07-19 11:30:14 -0300

    m

---
+++ c/ejemplos/R_GenHeapStruct.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+
+
+
+#define NUM_NODE_CLASSES 8
+#define NUM_OLD_GENERATIONS 2
+
+typedef unsigned int SEXPTYPE;
+typedef int R_len_t;
+
+
+struct sxpinfo_struct {
+    SEXPTYPE type      :  5;/* ==> (FUNSXP == 99) %% 2^5 == 3 == CLOSXP
+			     * -> warning: `type' is narrower than values
+			     *              of its type
+			     * when SEXPTYPE was an enum */
+    unsigned int obj   :  1;
+    unsigned int named :  2;
+    unsigned int gp    : 16;
+    unsigned int mark  :  1;
+    unsigned int debug :  1;
+    unsigned int trace :  1;  /* functions and memory tracing */
+    unsigned int spare :  1;  /* currently unused */
+    unsigned int gcgen :  1;  /* old generation number */
+    unsigned int gccls :  3;  /* node class */
+}; /*		    Tot: 32 */
+
+
+struct vecsxp_struct {
+    R_len_t	length;
+    R_len_t	truelength;
+};
+
+struct primsxp_struct {
+    int offset;
+};
+
+struct symsxp_struct {
+    struct SEXPREC *pname;
+    struct SEXPREC *value;
+    struct SEXPREC *internal;
+};
+
+struct listsxp_struct {
+    struct SEXPREC *carval;
+    struct SEXPREC *cdrval;
+    struct SEXPREC *tagval;
+};
+
+struct envsxp_struct {
+    struct SEXPREC *frame;
+    struct SEXPREC *enclos;
+    struct SEXPREC *hashtab;
+};
+
+struct closxp_struct {
+    struct SEXPREC *formals;
+    struct SEXPREC *body;
+    struct SEXPREC *env;
+};
+
+struct promsxp_struct {
+    struct SEXPREC *value;
+    struct SEXPREC *expr;
+    struct SEXPREC *env;
+};
+#define SEXPREC_HEADER \
+    struct sxpinfo_struct sxpinfo; \
+    struct SEXPREC *attrib; \
+    struct SEXPREC *gengc_next_node, *gengc_prev_node
+
+/* The standard node structure consists of a header followed by the
+   node data. */
+typedef struct SEXPREC {
+    SEXPREC_HEADER;
+    union {
+	struct primsxp_struct primsxp;
+	struct symsxp_struct symsxp;
+	struct listsxp_struct listsxp;
+	struct envsxp_struct envsxp;
+	struct closxp_struct closxp;
+	struct promsxp_struct promsxp;
+    } u;
+} SEXPREC, *SEXP;
+
+
+typedef union PAGE_HEADER {
+  union PAGE_HEADER *next;
+  double align;
+} PAGE_HEADER;
+
+static struct {
+    SEXP Old[NUM_OLD_GENERATIONS], New, Free;
+    SEXPREC OldPeg[NUM_OLD_GENERATIONS], NewPeg;
+#ifndef EXPEL_OLD_TO_NEW
+    SEXP OldToNew[NUM_OLD_GENERATIONS];
+    SEXPREC OldToNewPeg[NUM_OLD_GENERATIONS];
+#endif
+    int OldCount[NUM_OLD_GENERATIONS], AllocCount, PageCount;
+    PAGE_HEADER *pages;
+} R_GenHeap[NUM_NODE_CLASSES];
+
+
+void main () 
+{
+
+
+  //  printf("R_GenHeap[0].New.sxpinfo", 
+  printf("R_GenHeap[0]: %d\n", R_GenHeap[0].Free);
+}
