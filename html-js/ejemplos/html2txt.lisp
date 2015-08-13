6eb53273f971529b6ee6280f0303b591adfcfb16
smnicolas <nicolassanmartin1@gmail.com>
2015-08-12 23:42:50 -0300

    ordeno archivos

---
+++ html-js/ejemplos/html2txt.lisp
@@ -0,0 +1,8 @@
+(setq text '("á" "é" "í" "ó" "ú" "Á" "É" "Í" "Ó" "Ú" "ü" "Ü" "ñ" "Ñ" "¿" "¡"))
+(setq htmlt '("&aacute;" "&eacute;" "&iacute;" "&oacute;" "&uacute;" "&Aacute;" "&Eacute;" "&Iacute;" "&Oacute;" "&Uacute;" "&uuml;" "&Uuml;" "&ntilde;" "&Ntilde;" "&#191;" "&#161;"))
+
+(length text)
+(length htmlt)
+
+<br> 2 \n
+<p> 2 \n
