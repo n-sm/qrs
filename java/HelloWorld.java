a10abf11299ff0304ded7facefd8af52cabae432
smnicolas <nicolassanmartin1@gmail.com>
2015-10-01 13:55:22 -0300

    m

---
+++ java/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+
+}
+
+
+/*
+public class Test
+{
+  public static void main(String args[])
+  {
+    System.out.println("This is a test.");
+  }
+}
+*/
