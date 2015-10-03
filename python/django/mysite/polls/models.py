8c67163ba4328de4195bc0cffffd742be94b7c19
smnicolas <nicolassanmartin1@gmail.com>
2015-10-03 13:09:49 -0300

    m

--- python/django/mysite/polls/models.py
+++ python/django/mysite/polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+    def a_method():
+        print('say hello')
+    def otra_fun():
+        return 1
     
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -22,3 +26,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    def edit_text(text):
+        self.choice_text = text
+        
--- tcp-ip/get-IP-from-wlan.sh
+++ tcp-ip/get-IP-from-wlan.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # http://superuser.com/questions/261818/how-can-i-list-all-ips-in-the-connected-network-through-terminal-preferably
 sudo arp-scan --interface=wlan0 --localnet
+
+# en windows: ipconfig # linea 'Direccion IPv4'
