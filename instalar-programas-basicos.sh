1ce949d3b61c9b69f43b66a9254ec0dc9ab3ab56
n-sm <nsanmartin@psi.uba.ar>
2016-05-17 01:21:23 -0300

    rename html-js

---
+++ instalar-programas-basicos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# editores de texto
+sudo apt-get install emacs24 vim 
+# utilidades
+sudo apt-get install git pandoc catdoc mupdf
+# programacion
+sudo apt-get install r-base-core
+# red
+sudo apt-get install arp-scan
+# internet
+sudo apt-get install w3m elinks lynx
