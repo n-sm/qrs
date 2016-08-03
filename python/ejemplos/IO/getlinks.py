562d29a4b227d7745701495dd0791d53466ac938
n-sm <nsanmartin@psi.uba.ar>
2016-08-03 13:13:38 -0300

    getlinks.py:

--- python/ejemplos/IO/getlinks.py
+++ python/ejemplos/IO/getlinks.py
@@ -6,38 +6,64 @@ from bs4 import BeautifulSoup as bs
 
 def main(argv):
 
-    soup = getSoup(argv)
-    href_tags = soup.find_all(href=True)
-    print(soup)
-    for a in href_tags:
-        print(a)
-
-
-def getSoup(argv):
+    contentOn = False
+    numOn = False
+    
     html = ""
     try:
-        opts, args = getopt.getopt(argv, 'f:w:')
+        opts, args = getopt.getopt(argv, 'f:w:lcn')
     except getopt.GetoptError:
         print('getlinks [-f <filename> | -w <url> ]')
         sys.exit(2)
 
      
     if len(argv) == 0:
-        print("len arg es 0")
-        html = sys.stdin.readlines()
+        html = sys.stdin.read()
 
-    else:
-        for opt, arg in opts:
-            if opt == "-f":
-                with open (arg, 'r') as f:
-                    html = f.read()
-                    
-            elif opt == '-w':
-                response = urllib.request.urlopen(arg)
-                html = response.read()
+    for opt, arg in opts:
+        if opt == "-f":
+            with open (arg, 'r') as f:
+                html = f.read()
+
+        elif opt == '-w':
+            response = urllib.request.urlopen(arg)
+            html = response.read()
+
+        elif opt =='-c':
+            contentOn = True
+        elif opt == '-n':
+            numOn = True
                 
     soup = bs(html,"html.parser")
-    return(soup)
-                
+
+    href_tags = soup.find_all(href=True)
+    
+    contador = 1
+    for h in href_tags:
+        if numOn:
+            print("\n{}:".format(contador))
+            contador += 1
+        if contentOn:
+            if len(h.contents) != 0:
+                cont = str(h.contents[0])
+                print(breakLines(cont.strip()))
+            else:
+                print("Sin texto")
+        print(h.get('href'))
+
+    
+def breakLines(string, atMost=80):
+    l = atMost - 1
+    if(len(string) <= atMost): return string
+    else:
+        while (string[l] != " "):
+            l -= 1
+            if(l == 0): return("String de mas de 80 sin espacios?")
+
+        primera = string[:l]
+        segunda = breakLines(string[l+1:], atMost)
+        return(primera + "\n" + segunda)
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
