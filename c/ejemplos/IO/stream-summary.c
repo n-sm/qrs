f3ab97a810a11e8bc95c90d8ab5b8be08ad79f0c
n-sm <nicolassanmartin1@gmail.com>
2016-08-27 21:12:53 -0300

    stream summary: sort

--- c/ejemplos/IO/stream-summary.c
+++ c/ejemplos/IO/stream-summary.c
@@ -7,8 +7,10 @@
 #define D_ 128
 
 void printHelp();
-void orden (long long * desorden, long long * ordenada);
-
+void dameOrden (long long * desorden, int * ordenada);
+int cantidadNoNulos(long long * arr);
+int nOrden(long long i, long long * a);
+void printInOrder(long long * desorden, int * orden);
   
 int main( int argc, char *argv[] )  
 {
@@ -57,21 +59,24 @@ int main( int argc, char *argv[] )
       tabla[c - CHAR_MIN] += 1;
   }
 
+  int caracteresUsados = 0;
   for (int i = 0; i < 256; i++) {
     if (tabla[i] != 0) {
-      printf("dec: %d \tchar: %c \tcount: %lld\n",
-	     i + CHAR_MIN, i + CHAR_MIN, tabla[i]);
+      printf("%d: dec: %d \tchar: %c \tcount: %lld\n",
+	     ++caracteresUsados, i + CHAR_MIN, i + CHAR_MIN, tabla[i]);
     }
   }
 
-  long long  ordenada[257];
-  orden(tabla, ordenada);
-  long long * p = ordenada;
-  while(*p != -1) {
-    printf("%lld\t", *p++);
-  }
+  int orde[257];
+  orde[256]=-1;
+  dameOrden(tabla, orde);
+  
+  /* printf("%d\n", cantidadNoNulos(tabla)); */
+  /* for (int i = 0; i < 256; i++) { */
+  /*   if(tabla[i] != 0) printf("deso: %lld, ord: %d\n", tabla[i], orde[i]); */
+  /* } */
 
-  printf("\n");
+  printInOrder(tabla, orde);
   return 0;
 }
 
@@ -81,17 +86,94 @@ void printHelp()
   printf("\t-d decimal\n\t-o determina el orden\n");
 }
 
-void orden(long long * desorden, long long * ordenada) {
 
+int cantidadNoNulos(long long * arr) {
+
+  /*
+    Usamos como convencion que -1 indica el fin del arreglo, 
+    como cuando usamos '\0' para denotar el de una string.
+   */
+  
   int c = 0;
   long long * p;
-  p = desorden;
+  p = arr;
+  while(*p != -1) 
+    if (*p++ > 0) c++;
+  return c;
 
-  while(*p != -1) {
-    if (*p > 0) 
-      ordenada[c++] = *p;
-    p++;
-  }
-  ordenada[c] = -1;
 }
 
+int len(long long * arr) {
+  
+  int res = 0;
+  long long * p = arr;
+  while (*p++ != -1)
+    res++;
+  return res;
+}
+
+int nOrden(long long i, long long * arr) {
+  int res = 1;
+  long long *p = arr;
+  while(*p != -1) {
+    if (i < *p++) res++;
+  }
+  return res;
+    
+}
+
+
+void dameOrden(long long * desorden, int * orden) {
+  
+  long long * pdes = desorden;
+  int * pord = orden;
+
+  while(*pdes != -1) {
+    *pord++ = nOrden(*pdes++, desorden);
+  }
+  *pord = -1;
+  
+  /* printf("en dameOrden"); */
+  /* pord = orden; */
+  /* while(*pord != -1) { */
+  /*   printf("%d\t", *++pord); */
+  /* } */
+}
+  
+int max (int * arr) {
+
+  if (*arr == -1) abort();
+  int c = 0;
+  int res = *arr;
+  int * p = arr;
+  while (*p != -1) {
+    //printf("%d.", *p++);
+    if (res < *p) res = *p;
+    /* if (c++ > 9999999) { */
+    /*   printf("\nerror!: int * arr sin -1 final.\n"); */
+    /*   abort(); */
+    /* } */
+    p++;
+    if (p - arr > 258) {
+      printf("\nerror!: int * arr sin -1 final.2\n");
+      abort();
+    }
+  }
+  return res;
+}
+
+void printInOrder(long long * desorden, int * orden) {
+
+
+  int l = len(desorden);
+  int m = max(orden);
+  printf("\n---=-=-print in order\n");
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < l; j++) {
+      if (orden[j] == i)
+	printf("%c: %lld\n", j+CHAR_MIN, desorden[j]);
+    }
+  }
+  printf("\n");
+  return;
+}
