f88932e4286d7ae095ef16fe3be34b573dbd4635
n-sm <nsanmartin@psi.uba.ar>
2016-09-12 22:38:33 -0300

    get-size.c

--- c/ejemplos/bytetobinary.c
+++ c/ejemplos/bytetobinary.c
@@ -17,9 +17,10 @@
   (byte & 0x08 ? 1 : 0), \
   (byte & 0x04 ? 1 : 0), \
   (byte & 0x02 ? 1 : 0), \
-  (byte & 0x01 ? 1 : 0) 
-#define PRBIN(x) printf("%"# x " es "BYTETOBINPATTERN"\n"\
-  ,i , BYTETOBIN(x))
+  (byte & 0x01 ? 1 : 0)
+
+#define PRBIN(x) printf("%"# x " es "BYTETOBINPATTERN"\n",	\
+			x , BYTETOBIN(x))
 
 int main(int argc, char ** argv)
 {
